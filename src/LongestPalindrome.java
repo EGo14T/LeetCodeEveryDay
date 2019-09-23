41a2c86ff240cfba46ca1876199914fb1f9b626e
EGo14T <974412906@qq.com>
2019-09-23 09:22:25 +0800

    2019.9.23 LongestPalindrome中心扩展法

--- src/LongestPalindrome.java
+++ src/LongestPalindrome.java
@@ -3,7 +3,19 @@ import org.junit.Test;
 /**
  * Create By EGo1ST
  * Date ：Created in 2019/9/14 17:44
- * Description：
+ * Description：给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+ *
+ * 示例 1：
+ *
+ * 输入: "babad"
+ * 输出: "bab"
+ * 注意: "aba" 也是一个有效答案。
+ * 示例 2：
+ *
+ * 输入: "cbbd"
+ * 输出: "bb"
+ *
+ * 暴力解法通不过！！！时间复杂度太高
  */
 public class LongestPalindrome {
 
