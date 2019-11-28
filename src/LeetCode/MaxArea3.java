574ca94dbbfa955926ffa023835be88ab994a28f
EGo14T <974412906@qq.com>
2019-11-28 14:40:14 +0800

    2019.11.28 MaxArea双指针法

--- src/LeetCode/MaxArea3.java
+++ src/LeetCode/MaxArea3.java
@@ -5,6 +5,8 @@ package LeetCode;
  * Created by EGo1ST
  * Date：Created in 2019/11/28 14:30
  * Description: 双指针法
+ *
+ * 左右两个指针， 维护一个maxArea，当左指针指向的数字小于右指针指向的数字时， maxArea = height[左] * （右指针-左指针） 然后左指针右移；
  */
 public class MaxArea3 {
     public class Solution {
