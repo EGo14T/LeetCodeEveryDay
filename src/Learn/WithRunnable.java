2799074169880872b48bf5d0563cb0f34fd9b356
baiym5 <baiym5@chinaunicom.com>
2019-11-28 11:12:22 +0800

    2019.11.28 IsMatch

---
+++ src/Learn/WithRunnable.java
@@ -0,0 +1,26 @@
+package Learn;
+
+/**
+ * @author 王富昕
+ * Create By EGo1ST
+ * Date ：Created in 2019/9/30 14:52
+ * Description：
+ */
+public class WithRunnable implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("子线程启动,ID为:" + Thread.currentThread().getId() +
+                ",名字为" + Thread.currentThread().getName());
+    }
+
+    public static void main(String[] args)  {
+        // 创建一个线程并开启线程
+        Current a = new Current();
+        a.start();
+        // 多创建几个线程
+        new Current().start();
+        new Current().start();
+        new Current().start();
+
+    }
+}
